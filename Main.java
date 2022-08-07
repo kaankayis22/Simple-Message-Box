f812d0951540f0c9d0e7a569accb0a6249f81d63
Kaan Kayis <63751439+kaankayis22@users.noreply.github.com>
2022-08-07 11:27:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+class Main {
+  public static void main(String[] args) {
+
+    System.out.println("I love Allah(SWT)");
+    JOptionPane.showMessageDialog(null,"Contents of message", "Title of Box", JOptionPane.INFORMATION_MESSAGE);
+    
+  }
+}
